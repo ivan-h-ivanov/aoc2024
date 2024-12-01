f74ba00237bda26d0ea5f1c8496db4b95112f22f
ivan-h-ivanov <iivanov@telerik.com>
2024-12-01 17:02:00 +0200

    day1 both stars

---
+++ AdventOfCode2024/Day1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2024
+{
+    internal static class Day1
+    {
+        private static (List<int> left, List<int> right) Parse()
+        {
+            var lines = File.ReadAllLines("input1_1.txt");
+
+            var left = new List<int>(lines.Length);
+            var right = new List<int>(lines.Length);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i].Split();
+
+                left.Add(int.Parse(line[0]));
+                right.Add(int.Parse(line[3]));
+            }
+            return (left, right);
+        }
+
+        internal static int Star1()
+        {
+            var (left, right) = Parse();
+            return Task1(left, right);
+        }
+
+        internal static int Star2()
+        {
+            var (left, right) = Parse();
+            return Task2(left, right);
+        }
+
+        private static int Task1(List<int> left, List<int> right)
+        {
+            int aggr = 0;
+            left.Sort();
+            right.Sort();
+
+            for (int i = 0; i < left.Count; i++)
+            {
+                aggr += Math.Abs(right[i] - left[i]);
+            }
+
+            return aggr;
+        }
+
+        private static int Task2(List<int> left, List<int> right)
+        {
+            int aggr = 0;
+            var dict = right.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+            for (int i = 0; i < left.Count; i++)
+            {
+                var val = left[i];
+                var weightedVal = dict.ContainsKey(val) ? val * dict[left[i]] : 0;
+                aggr += weightedVal;
+            }
+
+            return aggr;
+        }
+    }
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string res;
+            Console.WriteLine("Enter date.star input");
+            while ((res = Console.ReadLine()) != null)
+            {
+                switch (res)
+                {
+                    case "1.1":
+                        Console.WriteLine(Day1.Star1());
+                        break;
+                    case "1.2":
+                        Console.WriteLine(Day1.Star2());
+                        break;
+                    default:
+                        Console.WriteLine("Format should be #D.#S, i.e. 4.2 for day 4, star 2");
+                        break;
+                }
+            }
+        }
+    }
+}
